b531b954dbf19f3c2029424494d381382e0e54ca
jonny <coorhook@gmail.com>
2025-01-15 17:49:13 +0800

    Update urodele.config.ts

--- urodele.config.ts
+++ urodele.config.ts
@@ -1,12 +1,12 @@
 export const config = {
   github: {
-    login: "glink25", // github login name, not user name
-    repo: "test-for-anything", //"urodele",
+    login: "jonnyan404", // github login name, not user name
+    repo: "jonnyan404.github.io", //"urodele",
     logInUrl: "",
     logInAuthUrl: "",
   },
   head: {
-    title: "Urodele",
+    title: "jonnyan404 の blog",
     brand: "Urodele",
     description: "A self-owned full-static blog system",
   },
@@ -17,7 +17,22 @@ export const config = {
   pagination: {
     size: 10,
   },
-  giscus: false as object | false,
+  giscus: {
+    src: "https://giscus.app/client.js",
+    "data-repo": "glink25/glink25.github.io",
+    "data-repo-id": "R_kgDONW6B_w",
+    "data-category": "Announcements",
+    "data-category-id": "DIC_kwDONW6B_84Cl-Rs",
+    "data-mapping": "pathname",
+    "data-strict": "0",
+    "data-reactions-enabled": "1",
+    "data-emit-metadata": "0",
+    "data-input-position": "bottom",
+    "data-theme": "preferred_color_scheme",
+    "data-lang": "zh-CN",
+    crossorigin: "anonymous",
+    async: true,
+  } as object | false,
 } as const;
 
 export default config;
